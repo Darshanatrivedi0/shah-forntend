9ab7a81dcec3f4d1985fb52239f03cc4e0a7fad9
Darshana <darshanat.webito@gmail.com>
2023-12-04 17:01:04 +0530

    Create login page

--- src/Components/AuthLayout.jsx
+++ src/Components/AuthLayout.jsx
@@ -1,8 +1,9 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 import { makeStyles } from "tss-react/mui";
 
 import backgroundImage from "../Assest/Images/authBack.png"
+import { Button } from '@mui/base';
 
 const useStyles = makeStyles()((theme) => {
     return {
@@ -15,6 +16,14 @@ const useStyles = makeStyles()((theme) => {
             justifyContent: 'center',
             alignItems: 'center',
         },
+        loginBox: {
+            background: '#FFF',
+            boxShadow: '0px 4px 20px 0px rgba(238, 238, 238, 0.50);',
+            maxWidth: '500px',
+            height: '500px',
+            border: '1px solid #F8F9FA',
+            borderRadius: '20px'
+        }
     };
 });
 
@@ -24,7 +33,14 @@ const AuthLayout = () => {
 
     return (
         <Box className={classes?.mainBox}>
-            login
+            <Box className={classes?.loginBox}>
+                <Typography style={{ fontSize: '22px', fontWeight: 600, textAlign: 'center', marginTop: '20px' }}>Logo</Typography>
+                <Typography style={{ fontSize: '45px', fontWeight: 600, textAlign: 'center', marginTop: '20px' }}>Login</Typography>
+                <Typography style={{ fontSize: '18px', fontWeight: 400, textAlign: 'center', color: '#737791', marginTop: '15px' }}>Please login to your account</Typography>
+                <Typography style={{ fontSize: '18px', fontWeight: 500, textAlign: 'center', marginTop: '20px', color: '#5D5FEF' }}>Forgot Password ?</Typography>
+
+            </Box>
+
         </Box>
     )
 }
